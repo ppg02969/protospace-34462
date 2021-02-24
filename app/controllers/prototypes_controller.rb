50c2eb3832bad4b1099153edf2f834e97e4a4154
ppg02969 <bibliomania76ex@gmail.com>
2021-02-24 19:08:18 +0900

    削除エラー修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -51,7 +51,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     prototype = Prototype.find(params[:id])
-    unless prototype.user_id == current_user 
+    unless prototype.user_id == current_user.id
       redirect_to action: :index
     end
   end
